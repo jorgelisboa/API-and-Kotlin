1b650b0004c70a5da553fb22149ecb2c32f06a8f
Jorge Lisboa <jorgelisboa659@gmail.com>
2021-12-31 20:04:35 -0300

    Layout almost finished.

---
+++ app/src/main/java/com/example/countryapp/EditDataActivity.kt
@@ -0,0 +1,11 @@
+package com.example.countryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_data)
+    }
+}
--- app/src/main/java/com/example/countryapp/MainActivity.kt
+++ app/src/main/java/com/example/countryapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.countryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.countryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        updateList()
+    }
+
+    private fun updateList() {
+
     }
 }
